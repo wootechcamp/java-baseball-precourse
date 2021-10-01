22a8d143cf7058fb0d8a96b10b3204ad84bf9a94
wook <mdir2@naver.com>
2021-10-02 01:00:45 +0900

    test(set-test): [요구사항3] 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는가?

--- src/test/java/learning/test/SetTest.java
+++ src/test/java/learning/test/SetTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -11,6 +12,7 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("학습테스트 - Set")
 public class SetTest {
 
     private Set<Integer> numbers;
@@ -36,8 +38,16 @@ public class SetTest {
 
     @ParameterizedTest(name = "[{index}] ParameterizedTest를 활용해 중복 코드를 제거 할수 있는가? number: {argumentsWithNames}")
     @ValueSource(ints = {1, 2, 3})
-    void contains(int number) {
+    void ParameterizedTest를_활용해_중복_코드를_제거_할수_있는가(int number) {
         //when, then
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest(name = "[{index}] 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는가? numbers: {argumentsWithNames}")
+    @CsvSource({"1,2,3,4,5"})
+    void _1과2그리고3_값은_contains_메소드_실행결과_true_4와5_값을_넣으면_false_가_반환되는가(int firstTrueNumber, int secondTrueNumber, int thirdTrueNumber, int firstFalseNumber, int secondFalseNumber) {
+        //when, then
+        assertThat(numbers).containsExactly(firstTrueNumber, secondTrueNumber, thirdTrueNumber);
+        assertThat(numbers).doesNotContainSubsequence(firstFalseNumber, secondFalseNumber);
     }
 }
