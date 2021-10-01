855f1fbfe3918be5a6834c70db2cfbf26adf2918
wook <mdir2@naver.com>
2021-10-02 00:18:15 +0900

    test(string-test): [요구사항3] `charAt()` 사용할 때 위치 값을 벗어나면 StringIndexOutOfBoundsException 발생하는가?

--- src/test/java/learning/test/StringTest.java
+++ src/test/java/learning/test/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DisplayName("학습테스트 - String Class")
 public class StringTest {
@@ -11,7 +12,7 @@ public class StringTest {
     private final static String DELIMITER = ",";
 
     @Test
-    @DisplayName("\"1,2\"을 `,`로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
+    @DisplayName("\"1,2\"을 `,`로 split 했을 때 1과 2로 잘 분리되는가?")
     void _1과2를_콤마로_split_했을_때_1과_2로_분리되는가() {
         //given
         final String csvValue = "1,2";
@@ -26,7 +27,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("\"1\"을 `,`로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스틑 구현한다.")
+    @DisplayName("\"1\"을 `,`로 split 했을 때 1만을 포함하는 배열을 반환하는가?")
     void _1을_콤마로_split_했을_때_1만_포함하는_배열이_반환되는가() {
         //given
         final String csvValue = "1";
@@ -41,7 +42,7 @@ public class StringTest {
     }
 
     @Test
-    @DisplayName("\"(1,2)\"값이 주어졌을 때 String의 substring() 메소드를 활용해 `()`을 제거하고 \"1,2\"를 반환하도록 구현한다.")
+    @DisplayName("\"(1,2)\"값이 주어졌을 때 \"1,2\"을 반환하는가?")
     void 소괄호가_포함_된_CSV값에서_소괄호를_제외_한_플레인한_CSV를_반환하는가() {
         //given
         final String csvValue = "(1,2)";
@@ -55,5 +56,18 @@ public class StringTest {
         assertThat(substring)
                 .as("`()`을 제거하고 1,2를 반환하는가?")
                 .isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("\"abc\"값이 주어졌을 때 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는가?")
+    void abc값을_charAt로_특정_위치의_문자를_가져올_때_위치_값이_벗어나면_StringIndexOutOfBoundsException이_발생하는가() {
+        //given
+        final String abc = "abc";
+        final int indexOutOfBounds = abc.length() + 1;
+
+        //when, then
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> abc.charAt(indexOutOfBounds))
+                .as("특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는가?");
     }
 }
