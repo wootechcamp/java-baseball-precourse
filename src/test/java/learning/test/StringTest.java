399ccebf8533d01144180aabdffe5c23c99335da
wook <mdir2@naver.com>
2021-10-01 23:52:28 +0900

    test(string-test): [요구사항1] "1"을 `,`로 split 했을 때 1만을 포함하는 배열을 반환하는가?

--- src/test/java/learning/test/StringTest.java
+++ src/test/java/learning/test/StringTest.java
@@ -21,4 +21,18 @@ public class StringTest {
                 .as("`,`로 split 했을 때 1과 2로 잘 분리 되는가?")
                 .contains("1", "2");
     }
+
+    @Test
+    @DisplayName("\"1\"을 `,`로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스틑 구현한다.")
+    void _1을_콤마로_split_했을_때_1만_포함하는_배열이_반환되는가() {
+        //given
+        final String csvValue = "1";
+
+        //when
+        final String[] dividedValues = csvValue.split(",");
+
+        assertThat(dividedValues)
+                .as("`,`로 split 했을 때 1만 포함하는가?")
+                .containsExactly("1");
+    }
 }
