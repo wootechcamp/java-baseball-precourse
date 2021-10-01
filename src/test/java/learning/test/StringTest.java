762bb4b6e2acdfcff78f2188df19a0e957d5fbc2
wook <mdir2@naver.com>
2021-10-02 00:04:57 +0900

    refactor(string-test): 구분 값 상수로 추출

--- src/test/java/learning/test/StringTest.java
+++ src/test/java/learning/test/StringTest.java
@@ -8,6 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("학습테스트 - String Class")
 public class StringTest {
 
+    private final static String DELIMITER = ",";
+
     @Test
     @DisplayName("\"1,2\"을 `,`로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.")
     void _1과2를_콤마로_split_했을_때_1과_2로_분리되는가() {
@@ -15,8 +17,9 @@ public class StringTest {
         final String csvValue = "1,2";
 
         //when
-        final String[] dividedValues = csvValue.split(",");
+        final String[] dividedValues = csvValue.split(DELIMITER);
 
+        //then
         assertThat(dividedValues)
                 .as("`,`로 split 했을 때 1과 2로 잘 분리 되는가?")
                 .contains("1", "2");
@@ -29,8 +32,9 @@ public class StringTest {
         final String csvValue = "1";
 
         //when
-        final String[] dividedValues = csvValue.split(",");
+        final String[] dividedValues = csvValue.split(DELIMITER);
 
+        //then
         assertThat(dividedValues)
                 .as("`,`로 split 했을 때 1만 포함하는가?")
                 .containsExactly("1");
