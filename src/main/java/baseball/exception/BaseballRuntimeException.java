5d99f44d370950aa8199cfaa792ea52610f2d8e3
wook <mdir2@naver.com>
2021-10-04 14:38:48 +0900

    fix(baseball-runtime-exception): 예외메시지 포맷 정상작동하도록 수정

--- src/main/java/baseball/exception/BaseballRuntimeException.java
+++ src/main/java/baseball/exception/BaseballRuntimeException.java
@@ -1,27 +1,29 @@
 package baseball.exception;
 
+import java.text.MessageFormat;
+
 public class BaseballRuntimeException extends RuntimeException {
     private static final String defaultMessage = "숫자야구게임 실행 중 오류가 발생했습니다.";
     protected static final String ERROR_MESSAGE_FORMAT = "[ERROR] {0}";
 
     public BaseballRuntimeException() {
-        super(String.format(ERROR_MESSAGE_FORMAT, defaultMessage));
+        super(MessageFormat.format(ERROR_MESSAGE_FORMAT, defaultMessage));
     }
 
     public BaseballRuntimeException(final String message) {
-        super(String.format(ERROR_MESSAGE_FORMAT, message));
+        super(MessageFormat.format(ERROR_MESSAGE_FORMAT, message));
     }
 
     public BaseballRuntimeException(final String message, final Throwable cause) {
-        super(String.format(ERROR_MESSAGE_FORMAT, message), cause);
+        super(MessageFormat.format(ERROR_MESSAGE_FORMAT, message), cause);
     }
 
     public BaseballRuntimeException(final Throwable cause) {
-        super(String.format(ERROR_MESSAGE_FORMAT, defaultMessage), cause);
+        super(MessageFormat.format(ERROR_MESSAGE_FORMAT, defaultMessage), cause);
     }
 
     public BaseballRuntimeException(final String message, final Throwable cause, final boolean enableSuppression,
         final boolean writableStackTrace) {
-        super(String.format(ERROR_MESSAGE_FORMAT, message), cause, enableSuppression, writableStackTrace);
+        super(MessageFormat.format(ERROR_MESSAGE_FORMAT, message), cause, enableSuppression, writableStackTrace);
     }
 }
